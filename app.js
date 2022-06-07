95f3811128c3bd3f02b8af30fd801423778c2366
wangshaoli <1125550225@qq.com>
2022-06-07 17:29:51 +0800

    多个错误处理

--- app.js
+++ app.js
@@ -26,12 +26,23 @@ app.use((request, response, next) => {
 
 // 错误处理函数
 app.use((error, request, response, next) => {
+   error +='错误处理1'
+    next(error) // 要把错误传给下一个 错误处理函数
+
+});
+
+
+// 错误处理函数
+app.use((error, request, response, next) => {
+    error +='错误处理2'
+
     if (response.headersSent) {
         return next(error);
     }
     response.status(500);
     response.send(error);
 });
+
 
 app.listen(3000, () => {
     console.log("listen 3000");
